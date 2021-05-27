3610c384d8da8e01077608cbb86a1bf4d7002da3
eungyeole <eungyeole@naver.com>
2021-05-27 23:20:47 +0900

    📑 :: [constances] depthsinfo 추가

---
+++ src/constances/depths/depthsinfo.ts
@@ -0,0 +1,13 @@
+import { IDepths } from "../../libs/interfaces";
+
+export const depthsinfo : Array<IDepths> = [
+    {
+        href : '/infinite',
+        name : '무한 스크롤'
+    },
+    {
+        href : '/page',
+        name : '페이지 네이션'
+    },
+    
+] 
