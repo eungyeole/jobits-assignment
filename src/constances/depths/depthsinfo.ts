b6865155b1557dbc26c5a8b22b16f0ebe170e58a
eungyeole <eungyeole@naver.com>
2021-05-29 13:54:56 +0900

    UPDATE ; depthsinfo

--- src/constances/depths/depthsinfo.ts
+++ src/constances/depths/depthsinfo.ts
@@ -2,12 +2,12 @@ import { IDepths } from "../../libs/interfaces";
 
 export const depthsinfo : Array<IDepths> = [
     {
-        href : '/infinite',
-        name : '무한 스크롤'
+        href : '/',
+        name : '페이지 네이션'
     },
     {
-        href : '/page',
-        name : '페이지 네이션'
+        href : '/infinite',
+        name : '무한 스크롤'
     },
     
 ] 
