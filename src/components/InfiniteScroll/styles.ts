9244ed474adac44cfcc598441130256e3ee41f9d
eungyeole <eungyeole@naver.com>
2021-05-29 13:53:47 +0900

    CREATE ; InfiniteScroll

---
+++ src/components/InfiniteScroll/styles.ts
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled'
+export const Wrapper = styled.div`
+    width: 90%;
+    margin: 0 auto;
+    padding: 50px 0;
+    & h3{
+        font-weight: 100;
+        margin-left: 15px;
+    }
+    
+`
