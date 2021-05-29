fdfefb36a2689381438e8caba3d1252a77ba3ec3
eungyeole <eungyeole@naver.com>
2021-05-29 13:55:06 +0900

    UPDATE ; useInfiniteScroll

--- src/hooks/useInfiniteScroll/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll/useInfiniteScroll.tsx
@@ -1,44 +1,79 @@
 import { AxiosPromise } from "axios";
-import { useCallback, useEffect, useState } from "react";
+import { LegacyRef, useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
+/*export function useInfiniteScroll(distance) : [number, any, LegacyRef<HTMLDivElement>] {
+  const scrollContainerRef = useRef<HTMLDivElement>();
+  const loaderRef = useRef();
+  const [page, setPage] = useState(0);
 
-export function useInfiniteScroll<T>(getData : (page : number) => AxiosPromise<any>): [T[], boolean, boolean]{
-  const [data, setData] = useState<T[]>([]);
-  const [page, setPage] = useState<number>(-1);
-  const [last, setLast] = useState<boolean>(false);
-  const [loading,setLoading] = useState<boolean>(true);
-  const scrollEvent = useCallback(()=>{
-    let scrollHeight = Math.max(
-      document.documentElement.scrollHeight,
-      document.body.scrollHeight  
-    );
-    let scrollTop = Math.max(
-      document.documentElement.scrollTop,
-      document.body.scrollTop
-    );
-    let clientHeight = document.documentElement.clientHeight;
-    if (scrollTop + clientHeight >= scrollHeight-200) { 
-      setLoading(true);
-    }
-  },[])
+  useLayoutEffect(() => {
+    const loaderNode = loaderRef.current;
+    const scrollContainerNode = scrollContainerRef.current;
+    if (!scrollContainerNode || !loaderNode) return;
+
+    const options = {
+      root: scrollContainerNode,
+      rootMargin: `0px 0px ${distance}px 0px`,
+    };
+
+    let previousY;
+    let previousRatio = 0;
+
+    const listener = entries => {
+      entries.forEach(
+        ({ isIntersecting, intersectionRatio, boundingClientRect = {} }) => {
+          const temp : any = boundingClientRect
+          const { y } = temp;
+          if (
+            isIntersecting &&
+            intersectionRatio >= previousRatio &&
+            (!previousY || y < previousY)
+          ) {
+            setPage(page => page + 1);
+          }
+          previousY = y;
+          previousRatio = intersectionRatio;
+        },
+      );
+    };
+
+    const observer = new IntersectionObserver(listener, options);
+    observer.observe(loaderNode);
+
+    return () => observer.disconnect();
+  }, [distance]);
+
+  return [page, loaderRef, scrollContainerRef];
+}*/
+
+export function useInfiniteScroll(){
+  const [isFetching, setIsFetching] = useState(false);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const intersectionCallbackFunc = entries => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting) {
+        console.log(entry.intersectionRatio)
+        if (entry.intersectionRatio >= 0.1) {
+          
+        }
+      }
+      else{
+        console.log('test2')
+      }
+    })
+  }
   useEffect(()=>{
-    window.addEventListener("scroll",scrollEvent)
-    return () => window.removeEventListener("scroll",scrollEvent);
-  },[])
-  useEffect(()=>{
-    if(loading && !last){
-      setPage((oldPage)=>{
-        getData(oldPage+1).then((res)=>{
-          if(res.data.length===0) setLast(true);
-          setData([...data, ...res.data])
-          setLoading(false);
-        })
-        .catch((e)=>console.log(e))
-        return oldPage+1;
-      }) 
-    }
-  },[loading])
-  useEffect(()=>{
-    if(last) window.removeEventListener("scroll",scrollEvent);
-  },[last])
-  return [data, loading, last];
+    const scrollObserver = new IntersectionObserver(intersectionCallbackFunc,{
+      root: null,
+      rootMargin : `0px`,
+      threshold: 1
+    })
+    scrollObserver.observe(scrollRef.current)
+    return () => scrollObserver?.disconnect();
+  },[scrollRef])
+  
+  // useEffect(()=>{
+  //   test.observe(scrollRef.current)
+  //   return () => test && test.disconnect();
+  // },[scrollRef]) 
+  return [scrollRef]
 }
