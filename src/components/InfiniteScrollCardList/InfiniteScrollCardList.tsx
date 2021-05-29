3e3bbf78ea2a9d022f78d5d0e904ae39b985f252
eungyeole <eungyeole@naver.com>
2021-05-29 22:55:23 +0900

    UPDATE ; infiniteScrollCardList style 수정

--- src/components/InfiniteScrollCardList/InfiniteScrollCardList.tsx
+++ src/components/InfiniteScrollCardList/InfiniteScrollCardList.tsx
@@ -1,15 +1,17 @@
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import { useInfiniteScroll } from "../../hooks/useInfiniteScroll/useInfiniteScroll";
 import InfiniteScrollCard from "../InfiniteScrollCard/InfiniteScrollCard";
-import recruitmentAPI from '../../libs/api/recruitment'
+import InfiniteScrollCardSkeleton from '../InfiniteScrollCard/InfiniteScrollCardSkeleton'
 import * as S from './styles'
+import { IRecruitmentData } from "../../libs/interfaces";
+import recruitment from "../../libs/api/recruitment";
+
 const InfiniteScrollCardList : FC = () => {
-    const [scrollRef] = useInfiniteScroll();
+    const [scrollRef, data, loading, last] = useInfiniteScroll<IRecruitmentData>(recruitment.getScroll);
     return(
         <S.CardWarpper ref={scrollRef}>
-            {
-                Array(12).fill(-1).map((i,index)=><InfiniteScrollCard key={index} {...i}></InfiniteScrollCard>)
-            }
+            { data.map((i,index)=><InfiniteScrollCard key={index} {...i}></InfiniteScrollCard>) }
+            { !last && loading && Array(12).fill(null).map((_i,index)=><InfiniteScrollCardSkeleton key={index}></InfiniteScrollCardSkeleton>)}
         </S.CardWarpper>
     )
 }
--- src/components/InfiniteScrollCardList/styles.ts
+++ src/components/InfiniteScrollCardList/styles.ts
@@ -3,4 +3,6 @@ export const CardWarpper = styled.div`
     display: flex;
     margin-top: 10px;
     flex-wrap: wrap;
+    height: 100%;
+    overflow: scroll;
 `
